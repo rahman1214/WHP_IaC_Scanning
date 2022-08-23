693b154c971ff706294b6b1c872e819b5b93a610
rahman1214 <67576804+rahman1214@users.noreply.github.com>
2022-08-23 19:31:53 -0400

    Update s3-4WHP.tf

--- IaC/terraform/storage/s3-4WHP.tf
+++ IaC/terraform/storage/s3-4WHP.tf
@@ -10,7 +10,7 @@ provider "aws" {
 resource "aws_s3_bucket" "safebucket" {
   provider = aws.central
   bucket   = "tf-test-bucket-source-12345"
-  acl      = "public-read"
+  acl      = "private"
 
   logging {
     target_bucket = aws_s3_bucket.log_bucket.id
